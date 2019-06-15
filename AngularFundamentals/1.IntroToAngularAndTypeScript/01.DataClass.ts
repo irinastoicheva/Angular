7d5471cc82f0672a2bbc3a56af8dacef3cfb3f4e
irinastoicheva <irinastoicheva@mail.bg>
2019-06-15 19:54:08 +0300

    commit

---
+++ AngularFundamentals/1.IntroToAngularAndTypeScript/01.DataClass.ts
@@ -0,0 +1,19 @@
+class Request11{
+    method;
+    url;
+    version;
+    message;
+    response;
+    fulfilled;
+    constructor(method: string, url: string, version: string, message: string){
+        this.method = method;
+        this.url = url;
+        this.version = version;
+        this.message = message;
+        this.response = undefined;
+        this.fulfilled = false;
+    }
+}
+
+let request = new Request11('GET', 'http://google.com', 'HTTP/1.1', '');
+console.log(request);
